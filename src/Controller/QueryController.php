8c73bf62dee7b32e68163f87e88404ad095d0dc4
bojms45 <bojms45@gmail.com>
2017-07-21 01:42:28 -0700

    Based on Raymond did, I simply correct some errors, and add 2 functions which are searchByPrice(could be filter later), searchByMediaID(probably unnecessary)

--- src/Controller/QueryController.php
+++ src/Controller/QueryController.php
@@ -2,20 +2,35 @@
 
 namespace App\Controller;
 
-use Cake\Controller\Controller;
+use Cake\Controller\AppController;
 use Cake\Event\Event;
 use Cake\Datasource\ConnectionManager;
 use Cake\ORM\TableRegistry;
 
-class QueryController extends Controller
+class QueryController extends AppController //should be AppController instead of Controller
 {
-
-    public function search() 
+    /**
+     * @return \Cake\Http\Response|null
+     * Find file by file name.
+     * Return all files that match the input "name".
+     */
+    public function initialize() 
+    {
+        
+    }
+    
+    public function seachBar()
+    {
+        
+    }
+    
+    
+    public function searchByName()
     {
         if($this->request->is('ajax')) {
             $this->autoRender = false;
-            $name = explode("=",$this->request->data["name"])[1];
-            $names = array();
+            $name = explode("=",$this->request->data["Title"])[1]; //change 'name' to 'Title'
+            $files = array();
             try {
                 $connection = ConnectionManager::get('default');
             }
@@ -28,10 +43,118 @@ class QueryController extends Controller
             
             foreach ($results as $row) 
             {
-                array_push($names, $row);
+                array_push($files, $row);
             }
-            $this->response->body(json_encode($names));
-
+            $this->response->body(json_encode($files));
+            return $this->response;
+        }
+    }
+    public function searchByCategory(){
+        if($this->request->is('ajax')){
+            $this->autoRender = false;
+            $category = explode("=",$this->request->data["Category"])[1]; //change 'category' to 'Category'
+            $files = array();
+            try{
+                $connection = ConnectionManager::get('default');
+            }
+            catch(Exception $error){
+                $this->log($error->getMessage(), 'debug');
+            }
+            $results = $connection  //this was unused
+                ->execute('SELECT * FROM Media, Categories 
+                            WHERE Categories_Category_ID=CategoryID AND Category=\'', $category,'\';')
+                ->fetchAll('assoc');
+            
+            /**add by Bo*/
+            foreach ($results as $row) 
+            {
+                array_push($files, $row);
+            }
+            $this->response->body(json_encode($files));
+            return $this->response;
+        }
+    }
+    /**
+     * @return \Cake\Http\Response|null
+     * Find users whose username and password match the input "username" and "password"
+     * Return all matching users.
+     */
+    
+    
+    public function searchByPrice() 
+    {   
+        if($this->request->is('ajax')) {
+            $this->autoRender = false;
+            $price = explode("=",$this->request->data["Price"])[1];
+            $files = array();
+            try {
+                $connection = ConnectionManager::get('default');
+            }
+            catch (Exception $error) {
+                $this->log($error->getMessage(), 'debug');
+            }
+            
+            $results = $connection  //this is unused
+                ->execute('SELECT * FROM Media WHERE Price=\'', $price,'\';')
+                ->fetchAll('assoc');
+            
+            foreach ($results as $row) 
+            {
+                array_push($files, $row);
+            }
+            $this->response->body(json_encode($files));
+            return $this->response;          
+        }        
+    }
+    
+    public function searchByMediaID()     //unnecessary
+    {
+        if($this->request->is('ajax')) {
+            $this->autoRender = false;
+            $mediaID = explode("=",$this->request->data["MediaID"])[1];
+            $files = array();
+            try {
+                $connection = ConnectionManager::get('default');
+            }
+            catch (Exception $error) {
+                $this->log($error->getMessage(), 'debug');
+            }
+            
+            $results = $connection  //this is unused
+                ->execute('SELECT * FROM Media, MediaAttributes
+                            WHERE MediaID=MediaID AND MediaID =\'', $mediaID,'\';')             
+                ->fetchAll('assoc');
+            
+            foreach ($results as $row) 
+            {
+                array_push($files, $row);
+            }
+            $this->response->body(json_encode($files));
+            return $this->response;          
+        }    
+    }
+    
+    
+    public function findUser(){
+        if($this->request->is('ajax')) {
+            $this->autoRender = false;
+            $username = explode("=",$this->request->data["Username"])[1];
+            $password = explode("=",$this->request->data["Password"])[1];
+            $user = array();
+            try {
+                $connection = ConnectionManager::get('default');
+            }
+            catch (Exception $error) {
+                $this->log($error->getMessage(), 'debug');
+            }
+            $results = $connection
+                ->execute('SELECT * FROM User WHERE Username=\'', $username, '\' AND Password=\'', $password, '\';')
+                ->fetchAll('assoc');
+            foreach ($results as $row)
+            {
+                array_push($user, $row);
+            }
+            $this->response->body(json_encode($user));
             return $this->response;
         }
     }
